5cabf2a6801e4246fb1d115e84386b3a9d6f15e1
kchalermsook <k.chalermsook@gmail.com>
2016-02-17 01:00:28 +0700

    finish get fanpage by page id

--- facebook-usercookie.js
+++ facebook-usercookie.js
@@ -1,6 +1,7 @@
-function getAllElementsWithAttribute(attribute) {
+var getAllElementsWithAttribute = function(doc, tagName, attribute) {
     var matchingElements = [];
-    var allElements = document.getElementsByTagName('*');
+    var allElements = doc.getElementsByTagName(tagName);
+    console.log(allElements);
     for (var i = 0, n = allElements.length; i < n; i++) {
         if (allElements[i].getAttribute(attribute) !== null) {
             // Element exists with attribute. Add to array.
@@ -10,54 +11,54 @@ function getAllElementsWithAttribute(attribute) {
     return matchingElements;
 }
 var cookieFilename = "cookie";
+var cookieFilename = "cookie";
 var fs = require('fs');
+var utils = require('utils');
 var casper = require('casper').create({
-    verbose: true,
-    logLevel: 'debug',
+    // verbose: true,
+    // logLevel: 'debug',
     pageSettings: {
         loadImages: false, //The script is much faster when this field is set to false
         loadPlugins: false,
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
     }
 });
-
+var data = fs.read(cookieFilename);
 //First step is to open Facebook
-casper.start().thenOpen("https://facebook.com", function() {
+casper.start();
+
+casper.then(function() {
+    phantom.cookies = JSON.parse(data);
+});
+
+casper.thenOpen("https://www.facebook.com/browse/?type=page_fans&page_id=760197027443228", function() {
     console.log("Facebook website opened");
 });
 
-
-//Now we have to populate username and password, and submit the form
-casper.then(function() {
-    console.log("Login using username and password");
-    this.evaluate(function() {
-        document.getElementById("email").value = "hideoaki@gmail.com";
-        document.getElementById("pass").value = "Ooaakk01";
-        document.getElementById("loginbutton").children[0].click();
-    });
-});
-
-//Wait to be redirected to the Home page, and then make a screenshot
 casper.then(function() {
     console.log("wait 6 secs for loading ");
-    var cookies = JSON.stringify(phantom.cookies);
-    fs.write(cookieFilename, cookies, 644);
-    this.wait(6000); //Wait a bit so page loads (there are a lot of ajax calls and that is why we are waiting 6 seconds)
-    // this.capture('AfterLogin.png');
+    this.wait(3000); //Wait a bit so page loads (there are a lot of ajax calls and that is why we are waiting 6 seconds)
+    this.capture('AfterLogin.png');
 });
-// open the page we want 
-casper.thenOpen("https://www.facebook.com/browse/?type=page_fans&page_id=760197027443228", function() {
-    console.log("Open to the page we want and wait 3 secs");
-    this.wait(3000);
 
-});
 
 casper.then(function() {
-    this.evaluate(function() {
-        users = getAllElementsWithAttribute("data-gt");
-        console.log("finish get all users");
-        console.log(users);
-    });
+    console.log("getAllElementsWithAttribute1");
+    docs = this.getElementsInfo('a[data-gt]:not([data-gt=""])');
+    var all = [];
+    for (var i = 0; i < docs.length; i++) {
+        attr = JSON.parse(docs[i].attributes["data-gt"]);
+        href = docs[i].attributes.href;
+        if (attr.engagement) {
+            // console.log(docs[i].attributes["data-gt"]);
+            all.push({
+                userId: attr.engagement.eng_tid,
+                href: href,
+                name: docs[i].text
+            });
+        }
+    }
+    utils.dump(all);
 });
 
 //Get all images greater than 100x100 pixels
